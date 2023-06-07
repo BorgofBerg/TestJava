d46cf35243028f3b7eec18f39a6de509b24d6f67
Alessandro <pigattoalessandro1@gmail.com>
2023-06-07 11:47:19 +0200

    Modifica6

--- src/it/betacom/main/testgit.java
+++ src/it/betacom/main/testgit.java
@@ -4,7 +4,8 @@ public class testgit {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Alessandro");	
+		System.out.println("Alessandro");
+		System.out.println("Fase 1 del Programma");
 		System.out.println("Fase 2 del Programma");
 
 	}
